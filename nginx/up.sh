9cef18e4c8ab0b3c5a15dc4816c0c73aea349e8c
Jiseong Bak <jiseongg301@gmail.com>
2024-05-26 19:44:57 +0900

    dockerize nginx

--- nginx/up.sh
+++ nginx/up.sh
@@ -2,7 +2,7 @@
 
 script_dir=$(cd $(dirname $0); pwd)
 
-docker run --rm -d -p 23260:20000 --name nginx \
+docker run --rm -d -p 20000:20000 --name nginx \
   -v $script_dir/configs:/etc/nginx:ro \
   -v $script_dir/nginx-log:/var/log/nginx:rw \
   nginx:1.26.0-alpine3.19-slim
