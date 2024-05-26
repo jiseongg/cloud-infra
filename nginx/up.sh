709118f7e66b0ff32d891b4351048c66f58c9f9b
Jiseong Bak <jiseongg301@gmail.com>
2024-05-26 14:51:52 +0900

    update structure

--- nginx/up.sh
+++ nginx/up.sh
@@ -2,8 +2,7 @@
 
 script_dir=$(cd $(dirname $0); pwd)
 
-docker run --rm -d -p 23260:80 --name nginx \
-  -v $script_dir/configs/jenkins.conf:/etc/nginx/jenkins.conf:ro \
-  -v $script_dir/configs/nginx.conf:/etc/nginx/nginx.conf:ro \
-  -v $script_dir/nginx-log:/var/log/nginx \
+docker run --rm -d -p 23260:20000 --name nginx \
+  -v $script_dir/configs:/etc/nginx:ro \
+  -v $script_dir/nginx-log:/var/log/nginx:rw \
   nginx:1.26.0-alpine3.19-slim
